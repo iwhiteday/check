6b2436c8594ba01fff78fd7820c367dadeb0c8ef
iwhiteday <idacascos@gmail.com>
2017-03-20 13:22:11 +0300

    hid keys and secrets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'MuMw3KPAG0i6azx5wRKZp0BeZ', 'UzI7W2CMQ46lkddnEc57egWmeCKdH3K63JPE1yomKjIc1tycrt'
+  provider :twitter, twitter_key, twitter_secret
 
-  provider :vkontakte, '5934437', 'nXx8BDXbg7Pu7tZ4tMPi'
+  provider :vkontakte, vk_id , vk_secret
 
-  provider :facebook, '1492107027490279', 'c39d7161a1ec8847a0f9922be0306505',
+  provider :facebook, facebook_id, facebook_secret
       scope: 'public_profile', info_fields: 'id,name,link'
 
 end
